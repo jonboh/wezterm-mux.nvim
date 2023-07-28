2448ea3b5e970c4936ef5c7f33c32caf601eed96
jonboh <jon.bosque.hernando@gmail.com>
2023-07-28 22:21:56 +0200

    use module functions

--- lua/wezterm-mux/init.lua
+++ lua/wezterm-mux/init.lua
@@ -1,8 +1,10 @@
+local M = {}
+
 function wezterm_cli_move(direction)
     vim.fn.system("wezterm cli activate-pane-direction "..direction)
 end
 
-function wezterm_move_left()
+function M.wezterm_move_left()
     local prev_win = vim.api.nvim_get_current_win()
     vim.cmd([[wincmd h]])
     local current_win= vim.api.nvim_get_current_win()
@@ -11,7 +13,7 @@ function wezterm_move_left()
     end
 end
 
-function wezterm_move_right()
+function M.wezterm_move_right()
     local prev_win = vim.api.nvim_get_current_win()
     vim.cmd([[wincmd l]])
     local current_win= vim.api.nvim_get_current_win()
@@ -20,7 +22,7 @@ function wezterm_move_right()
     end
 end
 
-function wezterm_move_up()
+function M.wezterm_move_up()
     local prev_win = vim.api.nvim_get_current_win()
     vim.cmd([[wincmd k]])
     local current_win= vim.api.nvim_get_current_win()
@@ -29,7 +31,7 @@ function wezterm_move_up()
     end
 end
 
-function wezterm_move_down()
+function M.wezterm_move_down()
     local prev_win = vim.api.nvim_get_current_win()
     vim.cmd([[wincmd j]])
     local current_win= vim.api.nvim_get_current_win()
@@ -38,8 +40,4 @@ function wezterm_move_down()
     end
 end
 
-vim.api.nvim_create_user_command("WeztermMoveLeft", wezterm_move_left, {})
-vim.api.nvim_create_user_command("WeztermMoveRight", wezterm_move_right, {})
-vim.api.nvim_create_user_command("WeztermMoveUp", wezterm_move_up, {})
-vim.api.nvim_create_user_command("WeztermMoveDown", wezterm_move_down, {})
-
+return M
